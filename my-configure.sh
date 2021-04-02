48812ce9139969ec525c017c0f09354c44ada27e
Karol Waszczuk <waszczukarol@gmail.com>
2021-04-02 16:50:38 +0200

    Adjust configuration script to avoid nvml support

--- my-configure.sh
+++ my-configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./configure --enable-debug --prefix=/usr --sysconfdir=/etc/slurm --with-mysql_config=/usr/bin  --libdir=/usr/lib64 --enable-multiple-slurmd
+./configure --enable-debug --prefix=/usr --sysconfdir=/etc/slurm --with-mysql_config=/usr/bin  --libdir=/usr/lib64 --enable-multiple-slurmd --without-nvml
